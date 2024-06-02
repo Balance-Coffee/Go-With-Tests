27ed75c9ef06680f594e8f9af921811bed71a7ea
Balance-Coffee <mailhemanth25@gmail.com>
2024-06-02 17:27:45 +0530

    refactored section

--- Hello World/hello.go
+++ Hello World/hello.go
@@ -2,35 +2,33 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	// if language == spanish {
-	// 	return spanishHelloPrefix + name
-	// }
+	return greetingPrefix(language) + name
+}
 
-	// if language == french {
-	// 	return frenchHelloPrefix + name
-	// }
-	// return englishHelloPrefix + name
-	prefix := englishHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
