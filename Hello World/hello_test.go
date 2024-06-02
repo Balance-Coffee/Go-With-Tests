d439505c4095691adf9a12f0f809c6c75efa4869
Balance-Coffee <mailhemanth25@gmail.com>
2024-06-02 17:25:15 +0530

    added responses for additional languages

--- Hello World/hello_test.go
+++ Hello World/hello_test.go
@@ -10,20 +10,41 @@ func TestHello(t *testing.T) {
 	// 	t.Errorf("got %q want %q", got, want)
 	// }
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+		got := Hello("Chris", "Spanish")
+		want := "Hola, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Mandy", "Spanish")
+		want := "Hola, Mandy"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mandy", "French")
+		want := "Bonjour, Mandy"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
